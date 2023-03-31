22cf8694d6ad2ed3df79c92d97fdb9ed0c82bd01
Ian Rosas <ian.rosas@academlo.com>
2023-03-30 19:54:35 -0600

    ssl config added

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -23,5 +23,6 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
   },
 };
